7f34a0fb766b09326adcee27d150bb828f3b9995
Ben Kimball <bkimball97@gmail.com>
2021-04-21 15:38:11 -0500

    initial commit

---
+++ practice.js
@@ -0,0 +1,22 @@
+/* const calculateAgeInDogYears = (ageOfPerson) => {
+    const ageInDogYears = ageOfPerson / 7
+    
+    return ageInDogYears
+}
+
+const dogAge = calculateAgeInDogYears(45)
+console.log(dogAge) */
+
+const dog = "corgi"
+
+const favoriteDogBreed = (dog) => {
+    const myFavorite = dog()
+
+    for (const dog of dogs) {
+        if ()
+
+        console.log(`When it comes to pets, ${}`)
+    }    
+}
+
+favoriteDogBreed()
